819035644a2faf57ae3b1ac662b38f0414b879bc
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-05-08 02:18:30 +0900

    1.3.5-softmaxwitloss layer

---
+++ common/functions.py
@@ -0,0 +1,40 @@
+from common.np import *
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def softmax(x):
+    if x.ndim == 2:
+        # overflow를 막기 위해 입력값 중 
+        # 최대값을 빼준다. >> 밑러닝-1, 3.5.2 참고
+        x = x - x.max(axis=1, keepdims=True)
+        x = np.exp(x)
+        x /= x.sum(axis=1, keepdims=True)
+    elif x.ndim == 1:
+        x = x - np.max(x)
+        x = np.exp(x) / np.sum(np.exp(x))
+
+    return x
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    # 정답 데이터가 원핫 벡터일 경우 정답 레이블 인덱스로 변환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    
+    batch_size = y.shape[0]
+
+    cross_entropy = np.log(y[np.arange(batch_size), t] + 1e-7)
+    loss = -np.sum(cross_entropy) / batch_size
+    
+    return loss
---
+++ common/layers.py
@@ -0,0 +1,66 @@
+from common.np import *
+from common.config import GPU
+from common.functions import softmax, cross_entropy_error
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+        
+    def forward(self, x):
+        W, = self.params
+        out = np.matmul(x, W)
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, = self.params
+        dx = np.matmul(dout, W.T)
+        dW = np.matmul(self.x.T, dout)
+        self.grads[0][...] = dW  # 깊은 복사
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+        
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmax의 출력
+        self.t = None  # 정답 레이블
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 정답 레이블이 원핫 벡터일 경우 정답의 인덱스로 변환
+        # cross_entropy_error()에 이미 있어서 굳이 필요 없을 듯
+        if self.t.size == self.y.size:
+            self.t = self.t. argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx /= batch_size
+
+        return dx

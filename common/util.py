74df378083794ecc63e4742a996d13fc4eff5deb
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-05-10 23:49:11 +0900

    2.3.4 Co-occurrence Matrix

--- common/util.py
+++ common/util.py
@@ -4,6 +4,48 @@ import os
 from common.np import *
 
 
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id =  len(word_to_id)
+            word_to_id[word] = new_id
+            
+    id_to_word = {id_: word for word, id_ in word_to_id.items()}
+    
+    corpus = np.array([word_to_id[word] for word in words])
+    return corpus, word_to_id, id_to_word
+
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    '''동시발생 행렬 생성
+    :param corpus: 말뭉치(단어 ID 목록)
+    :param vocab_size: 단어 수
+    :param window_size: 윈도우 크기(윈도우 크기가 1이면 타깃 단어 좌우 한 단어씩이 맥락에 포함)
+    :return: 동시발생 행렬
+    '''
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+    
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i  # left window_size
+            right_idx = idx + i  # right window_size
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+                
+    return co_matrix
+
 def clip_grads(grads, max_norm):
     total_norm = 0
     for grad in grads:

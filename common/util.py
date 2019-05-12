6dd618d6aefa850b5cd8edf61c24eee521975ee0
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-05-13 01:00:23 +0900

    2.4 ing...

--- common/util.py
+++ common/util.py
@@ -21,6 +21,53 @@ def preprocess(text):
     return corpus, word_to_id, id_to_word
 
 
+def cos_similarity(x, y, eps=1e-8):
+    '''코사인 유사도 산출
+    :param x: 벡터
+    :param y: 벡터
+    :param eps: '0으로 나누기'를 방지하기 위한 작은 값
+    :return:
+    '''
+    nx = x / np.sqrt(np.sum(x**2) + eps)  # x의 정규화
+    ny = y / np.sqrt(np.sum(y**2) + eps)  # y의 정규화
+    return np.dot(nx, ny)
+
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    '''유사 단어 검색
+    :param query: 쿼리(텍스트)
+    :param word_to_id: 단어에서 단어 ID로 변환하는 딕셔너리
+    :param id_to_word: 단어 ID에서 단어로 변환하는 딕셔너리
+    :param word_matrix: 단어 벡터를 정리한 행렬. 각 행에 해당 단어 벡터가 저장되어 있다고 가정한다.
+    :param top: 상위 몇 개까지 출력할 지 지정
+    '''
+    # 1) 검색어를 꺼낸다.
+    if query not in word_to_id:
+        print(f'{query}(을)를 찾을 수 없습니다.')
+        return 
+    
+    print(f'\n[query] {query}')
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+    
+    # 2) 코사인 유사도 계산
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+        
+    # 3) 코사인 유사도를 기준으로 내림차순으로 출력
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(f' {id_to_word[i]}: {similarity[i]}')
+        
+        count +=1
+        if count >= top:
+            return
+
+
 def create_co_matrix(corpus, vocab_size, window_size=1):
     '''동시발생 행렬 생성
     :param corpus: 말뭉치(단어 ID 목록)

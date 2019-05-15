61e13c4f77346bd16e2f642979c4edb45bd31971
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-05-15 19:10:36 +0900

    3.2 완료

---
+++ Chap03-Word2Vec/cbow_predict.py
@@ -0,0 +1,27 @@
+# CBOW모델의 추론 처리 구현
+# chap03/cbow_predict.py
+import sys
+sys.path.append('..')
+import numpy as np
+from common.layers import MatMul
+
+# 샘플 맥락 데이터
+c0 = np.array([[1, 0, 0, 0, 0, 0, 0]])
+c1 = np.array([[0, 0, 1, 0, 0, 0, 0]])
+
+# 가중치 초기화
+W_in = np.random.randn(7, 3)
+W_out = np.random.randn(3, 7)
+
+# 계층 생성
+in_layer0 = MatMul(W_in)
+in_layer1 = MatMul(W_in)
+out_layer = MatMul(W_out)
+
+# 순전파
+h0 = in_layer0.forward(c0)
+h1 = in_layer1.forward(c1)
+h = 0.5 * (h0 + h1)  # average
+s = out_layer.forward(h)  # score
+
+print(s)

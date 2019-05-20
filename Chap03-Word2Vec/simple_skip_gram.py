6b643368771591d1f2add0032a1bd802fa3c4b93
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-05-20 22:17:54 +0900

    chap03. Word2Vec 완료

---
+++ Chap03-Word2Vec/simple_skip_gram.py
@@ -0,0 +1,47 @@
+# chap03/simple_skip_gram.py
+import sys
+sys.path.append('..')
+import numpy as np
+from common.layers import MatMul, SoftmaxWithLoss
+
+
+class SimpleSkipGram:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+        
+        # 가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H, V).astype('f')
+        
+        # 레이어 생성
+        self.in_layer = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer1 = SoftmaxWithLoss()
+        self.loss_layer2 = SoftmaxWithLoss()
+        
+        # 모든 가중치와 기울기를 리스트에 모은다.
+        layers = [self.in_layer, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+            
+        # 인스턴스 변수에 단어의 분산표현을 저장한다.
+        self.word_vecs1 = W_in
+        self.word_vecs2 = W_out.T
+        
+    def forward(self, contexts, target):
+        h = self.in_layer.forward(target)
+        s = self.out_layer.forward(h)
+        l1 = self.loss_layer1.forward(s, contexts[:, 0])
+        l2 = self.loss_layer2.forward(s, contexts[:, 1])
+        loss = l1 + l2
+        return loss
+    
+    def backward(self, dout=1):
+        dl1 = self.loss_layer1.backward(dout)
+        dl2 = self.loss_layer2.backward(dout)
+        ds = dl1 + dl2
+        dh = self.out_layer.backward(ds)
+        self.in_layer.backward(dh)
+        return None

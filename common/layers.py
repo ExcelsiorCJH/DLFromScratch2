602979696ee0400cd4ff922c8d60bdef5277def0
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-05-21 16:59:06 +0900

    4.1 Embedding Layer

--- common/layers.py
+++ common/layers.py
@@ -89,3 +89,22 @@ class SoftmaxWithLoss:
         dx /= batch_size
 
         return dx
+
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+        
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout)
+        return None

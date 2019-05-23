b18cbef1308abb0e2db6406832fd145d94763ef5
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-05-24 01:55:00 +0900

    4.2.4 완료

---
+++ Chap04-Word2Vec_Improved/negative_sampling_layer.py
@@ -0,0 +1,29 @@
+# Chap04/negative_sampling_layer.py
+import sys
+sys.path.append('..')
+import collections
+from common.np import *
+from common.layers import Embedding, SigmoidWithLoss
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+        
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+        
+        self.cache = (h, target_W)
+        return out
+    
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+        
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * target_W
+        return dh
--- common/layers.py
+++ common/layers.py
@@ -62,6 +62,28 @@ class Sigmoid:
         return dx
 
 
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None  # sigmoid의 출력
+        self.t = None  # 정답 데이터
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
+
+
 class SoftmaxWithLoss:
     def __init__(self):
         self.params, self.grads = [], []

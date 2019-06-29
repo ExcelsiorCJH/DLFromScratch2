f52dcbbe8e48c51791815d876083469d1caf5c13
ExcelsiorCJH <excelsiorcjh@gmail.com>
2019-06-29 23:06:02 +0900

    TimeRNN class

--- common/time_layers.py
+++ common/time_layers.py
@@ -11,7 +11,7 @@ class RNN:
         self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
         self.cache = None  # 역전파에 사용할 중간 데이터
 
-    def forward(self, x, h, h_prev):
+    def forward(self, x, h_prev):
         Wx, Wh, b = self.params
         t = np.matmul(h_prev, Wh) + np.matmul(x, Wx) + b
         h_next = np.tanh(t)
@@ -35,3 +35,62 @@ class RNN:
         self.grads[2][...] = db
 
         return dx, dh_prev
+
+
+class TimeRNN:
+    def __init__(self, Wx, Wh, b, stateful=False):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.layers = None  # RNN 계층을 리스트로 저장
+        
+        self.h, self.dh = None, None
+        self.stateful = stateful
+        
+    def set_state(self, h):
+        '''hidden state(h)를 설정하는 메서드'''
+        self.h = h
+    
+    def reset_state(self):
+        '''hidden state(h)를 초기화하는 메서드'''
+        self.h = None
+        
+    def forward(self, xs):
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape  # N(batch), T(time steps), D(input size)
+        D, H = Wx.shape
+        
+        self.layers = []
+        hs = np.empty((N, T, H), dtype='f')
+        
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N, H), dtype='f')
+            
+        for t in range(T):
+            layer = RNN(*self.params)
+            self.h = layer.forward(xs[:, t, :], self.h)
+            hs[:, t, :] = self.h
+            self.layers.append(layer)
+            
+        return hs
+    
+    def backward(self, dhs):
+        Wx, Wh, b = self.params
+        N, T, H = dhs.shape
+        D, H = Wx.shape
+        
+        dxs = np.empty((N, T, D), dtype='f')
+        dh = 0
+        grads = [0, 0, 0]
+        for t in reversed(range(T)):
+            layer = self.layers[t]
+            dx, dh = layer.backward(dhs[:, t, :] + dh)  # 합산된 기울기
+            dxs[:, t, :] = dx
+            
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad
+                
+        for i, grad in enumerate(grads):
+            self.grads[i][...] = grad
+        self.dh = dh
+        
+        return dxs
